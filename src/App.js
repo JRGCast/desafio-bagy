660e4f3de7ed689ec199164dda8b716c33cb7b1b
JRGCast <joao.ralph1@gmail.com>
2021-12-27 08:31:02 -0300

    navBar adicionada ao app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import NavBar from './components/NavBar';
 import AppRoutes from './routes/AppRoutes';
 import './sass/main.scss';
 
 function App() {
   return (
-   <main>
-     <AppRoutes/>
-   </main>
+    <main>
+      <NavBar />
+      <AppRoutes />
+    </main>
   );
 }
 
