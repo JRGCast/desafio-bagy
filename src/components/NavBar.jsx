1eb78597cd5b87073c9ba3c822022dd83e9a69b0
JRGCast <joao.ralph1@gmail.com>
2021-12-27 08:29:48 -0300

    testing navLink in netlify

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,10 @@
-const NavBar = () => {
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
+const NavBar = () => {
+<ul>
+  <NavLink to='/a'>OtherPage</NavLink>
+  <NavLink to='/'>LPage</NavLink>
+</ul>
 }
 
 export default NavBar
