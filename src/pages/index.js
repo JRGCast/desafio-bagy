2b2b80afc8d2c0e5ea3d623cf13192f0c9fffb9b
JRGCast <joao.ralph1@gmail.com>
2021-12-27 16:03:16 -0300

    adicionada notFound e componente exportador generico (index.js em components)

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+export { default as Clients } from './Clients';
+export { default as Configurations } from './Configurations';
+export { default as GeneralVision } from './GeneralVision';
+export { default as NotFound } from './NotFound';
+export { default as PlansNAchvmts } from './PlansNAchvmts';
+export { default as Products } from './Products';
+export { default as Sales } from './Sales';
+export { default as Stores } from './Stores';
+
