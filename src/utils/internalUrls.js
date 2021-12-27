81ae1345141ac559116bbff04fa021b2e7045d79
JRGCast <joao.ralph1@gmail.com>
2021-12-27 15:36:42 -0300

    criadas urls internas de acordo com o figma, com as respectivas paginas, ainda sem conteudo

--- src/utils/internalUrls.js
+++ src/utils/internalUrls.js
@@ -1,7 +1,21 @@
 export const landingPageUrl = '/';
 export const otherPageUrl = '/a';
+export const generalVUrl = '/visaogeral';
+export const storesUrl = '/lojas';
+export const salesUrl = '/vendas';
+export const clientsUrl = '/clientes';
+export const productsUrl = '/produtos';
+export const planNAchvmntsUrl = '/planosemetas';
+export const configsUrl = '/configuracoes';
 
 export const allUrlsArr = [
   { name: 'Landing Page', path: '/' },
-  { name: 'Other Page', path: '/a' }
+  { name: 'Other Page', path: '/a' },
+  { name: 'Visão Geral', path: generalVUrl },
+  { name: 'Lojas', path: storesUrl },
+  { name: 'Vendas', path: salesUrl },
+  { name: 'Clientes', path: clientsUrl },
+  { name: 'Produtos', path: productsUrl },
+  { name: 'Planos e Metas', path: planNAchvmntsUrl },
+  { name: 'Configurações', path: configsUrl }
 ];
