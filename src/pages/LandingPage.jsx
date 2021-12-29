868e108a0fef2297d3cb4403d624dfa0c3eeaed9
JRGCast <joao.ralph1@gmail.com>
2021-12-29 18:00:10 -0300

    modificado texto da landingpage

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,27 +1,10 @@
-import RenderLineChart from "../components/GraphicChart";
 
 const LandingPage = () => {
   return (
-    <div>
+    <div style={ { textAlign: 'center' } }>
       <h1>LandingPage</h1>
-      <p style={ { width: '20%' } }>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempor, dui a semper hendrerit, massa augue tempus tortor, vel scelerisque odio lorem in dolor. Ut semper massa nec arcu fermentum sagittis et sit amet felis. Nulla nibh nunc, facilisis ac semper nec, pharetra ornare turpis. Praesent sapien libero, auctor ac neque ut, commodo vulputate urna. Aliquam mauris turpis, accumsan quis ornare quis, blandit vitae sapien. Donec ac elementum enim. Etiam lacinia, leo nec dictum varius, ante ipsum pharetra neque, sit amet cursus sapien lorem ac odio. Duis non fringilla neque. Duis et erat tellus. Morbi facilisis orci orci, cursus tincidunt tellus consequat id. Maecenas tempor tellus at dignissim varius. Curabitur rhoncus ex id ipsum tempor, id hendrerit lorem maximus.
-
-        Praesent dolor augue, molestie vel mi sit amet, fringilla elementum dui. Etiam orci sem, fermentum eu lectus in, rhoncus ornare ex. Duis dui neque, laoreet nec ex eu, rutrum tincidunt sem. Nam eget eros vel velit cursus porta ac id eros. Vivamus sed aliquam odio. Morbi et quam eget libero pretium tempor. In malesuada lectus finibus quam vulputate, ut convallis lectus mollis. Mauris id velit quis leo vestibulum ornare eget non nisi. Donec molestie ultrices ipsum a eleifend. Integer venenatis scelerisque ipsum, id tincidunt orci lobortis ac.
-
-        Morbi vitae velit nunc. Pellentesque aliquam elit justo, at aliquet augue mollis pretium. Proin non tempor dui, a fermentum diam. Morbi velit ipsum, pellentesque ac tincidunt et, semper at lorem. In maximus libero sapien, sed ultricies justo elementum ac. Suspendisse in nunc dapibus, dapibus sem eget, pretium elit. Phasellus ligula ligula, mattis ut nibh quis, consequat eleifend neque.
-
-        In lacinia nibh at justo molestie volutpat. Integer non ex eu ante viverra consectetur nec in quam. Nullam tempor erat nec urna mattis tincidunt. In diam sapien, efficitur at mi a, dictum feugiat enim. Morbi sollicitudin leo ac ante pulvinar faucibus. Praesent eget efficitur tellus. Etiam fringilla nunc lacus, eu ornare velit porttitor in. Pellentesque in dui ut orci aliquet laoreet a vitae sem. Nulla at sollicitudin metus. Sed vulputate metus at ligula sodales, non dictum ex faucibus. Proin tempus venenatis mauris sed accumsan. Donec quis pharetra eros.
-
-        Proin iaculis sit amet nisi ut vestibulum. Ut rutrum malesuada elit, in bibendum augue rutrum id. Donec in ligula vitae lectus pretium pretium a vitae massa. Quisque sed massa vel mi condimentum pretium et vel dolor. Quisque condimentum vestibulum ante eget sollicitudin. Mauris sed feugiat sapien, non vehicula lorem. Nulla a nisl sed urna convallis laoreet.
-
-        Vivamus vel felis leo. Proin fermentum, ipsum sit amet pretium scelerisque, diam dui imperdiet justo, tempus tincidunt orci nulla a diam. Pellentesque iaculis ante justo, id convallis erat interdum sit amet. Integer consequat non sapien sed cursus. Donec semper augue eget enim fermentum fermentum eu at neque. Duis pulvinar rutrum elementum. Ut eleifend ac lectus id maximus. Etiam ornare pulvinar congue. Duis tincidunt quam eu mauris vehicula, et pulvinar justo interdum. Fusce feugiat posuere ullamcorper. Nam porta sit amet nibh non ullamcorper.
-
-        Morbi in ligula ligula. Donec sed urna placerat est vulputate feugiat. Aliquam quis scelerisque sem. Phasellus nec neque ac nisi placerat vulputate sed vel libero. Sed eu laoreet ante. Morbi vitae ligula at sapien luctus ultricies. Morbi pharetra augue eu aliquet pretium. Nullam tempus tempus gravida. Donec rhoncus tempor tellus, eget placerat sem vehicula in. Etiam ornare nunc nisl, sed consectetur metus euismod ut. Suspendisse sagittis tortor turpis, a cursus odio interdum ut.
-
-        Praesent sit amet libero cursus, vestibulum mauris in, gravida turpis. Mauris efficitur lectus id turpis ornare, ut ultricies turpis euismod. Sed vitae faucibus risus. Nullam faucibus non elit eget euismod. Ut odio quam, sodales eu auctor a, iaculis vitae massa. Cras felis elit, placerat nec turpis sed, feugiat pharetra enim. Pellentesque hendrerit nisl quis hendrerit porta.
-
-        Generated 8 paragraphs, 619 words, 4036 bytes of Lorem Ipsum</p>
-      <RenderLineChart></RenderLineChart>
+      <p> Lamento, realmente não consegui cumprir com o desafio em tempo hábil. A bagy parece ser uma excelente empresa, mas não estou a altura.</p>
+      <p>Peço que assim que terminar a análise, quando possível, me notifique, notei muitas falhas no meu conhecimento e quero melhorar utilizando esse desafio</p>
     </div>
   );
 };
