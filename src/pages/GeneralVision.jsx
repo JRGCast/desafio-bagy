62d195aab86c5fc30d58f3e3fc9f8788b6019df4
JRGCast <joao.ralph1@gmail.com>
2021-12-29 11:27:46 -0300

    dificuldade com .map e promises ok, componente generalVision pegando os dados

--- src/pages/GeneralVision.jsx
+++ src/pages/GeneralVision.jsx
@@ -2,39 +2,48 @@ import { useEffect, useState } from "react";
 import BottomCards from "../components/BottomCards";
 import MiddleCards from "../components/MiddleCards";
 import TopCards from "../components/TopCards";
-import {
-  DB_GRAPHQL_URL, LOJA_1_TOKEN, LOJA_2_TOKEN,
-  LOJA_3_TOKEN, LOJA_4_TOKEN, LOJA_5_TOKEN
-} from '../utils/externalUrls';
+import { DB_GRAPHQL_URL, LOJAS_TOKENS } from '../utils/externalUrls';
 import getData from '../utils/getData';
-import { getStorageNameQuery } from '../utils/queryTypes';
+import { getAllQuery, getStorageNameQuery } from '../utils/queryTypes';
 
+const {
+  LOJA_1_TOKEN, LOJA_2_TOKEN,
+  LOJA_3_TOKEN, LOJA_4_TOKEN, LOJA_5_TOKEN } = LOJAS_TOKENS;
 const GeneralVision = () => {
   const [lojasNames, setLojasNames] = useState([]);
   const [lojasData, setLojasData] = useState([]);
 
   const puttingDataInsideObj = async (obj, data) => ({ [obj]: data });
 
-  // useEffect(() => {
-  //   const promisingNames = async () =>
-  //     Promise.all([
-  //       Promise.resolve(getData(DB_GRAPHQL_URL, LOJA_1_TOKEN, getStorageNameQuery)),
-  //       Promise.resolve(getData(DB_GRAPHQL_URL, LOJA_2_TOKEN, getStorageNameQuery)),
-  //       Promise.resolve(getData(DB_GRAPHQL_URL, LOJA_3_TOKEN, getStorageNameQuery)),
-  //       Promise.resolve(getData(DB_GRAPHQL_URL, LOJA_4_TOKEN, getStorageNameQuery)),
-  //       Promise.resolve(getData(DB_GRAPHQL_URL, LOJA_5_TOKEN, getStorageNameQuery)),])
-  //       .then((values) => { setLojasNames(values); });
-  //   promisingNames();
-  // }, []);
-  // useEffect(() => {
-  //   const promisingObjs = () => lojasNames.forEach(({ data: { me: { name } } }, index) => {
-  //     console.log(name, `LOJA_${index + 1}_TOKEN`);
-  //   });
-  //   promisingObjs();
-  // }, [lojasNames]);
+  useEffect(() => {
+    const promisingNames = async () =>
+      Promise.all([
+        Promise.resolve(getData(DB_GRAPHQL_URL, LOJA_1_TOKEN, getStorageNameQuery)),
+        Promise.resolve(getData(DB_GRAPHQL_URL, LOJA_2_TOKEN, getStorageNameQuery)),
+        Promise.resolve(getData(DB_GRAPHQL_URL, LOJA_3_TOKEN, getStorageNameQuery)),
+        Promise.resolve(getData(DB_GRAPHQL_URL, LOJA_4_TOKEN, getStorageNameQuery)),
+        Promise.resolve(getData(DB_GRAPHQL_URL, LOJA_5_TOKEN, getStorageNameQuery)),])
+        .then((values) => { setLojasNames(values); });
+    promisingNames();
+  }, []);
+  useEffect(() => {
+    const promisingDatas = async () => {
+      const mapping = await Promise.all(
+        lojasNames.map(async (item, index) => {
+          const storeName = item.data?.me?.name.replaceAll(' ', '_');
+          const currToken = LOJAS_TOKENS[`LOJA_${index + 1}_TOKEN`];
+          const storeData = await Promise.resolve(getData(DB_GRAPHQL_URL, currToken, getAllQuery));
+          const joiningObj = puttingDataInsideObj(storeName, storeData);
+          return joiningObj;
+        })
+      );
+      setLojasData(mapping);
+    };
+    promisingDatas();
+  }, [lojasNames]);
+
   return (
     <div className='GeneralVision-main-wrapper'>
-    {console.log(Object.values(LOJA_1_TOKEN))}
       <header>
         <h1>Página Visão Geral</h1>
         <TopCards />
--- src/utils/externalUrls.js
+++ src/utils/externalUrls.js
@@ -2,7 +2,7 @@ export const DB_GRAPHQL_URL = process.env.REACT_APP_BAGY_GRAPH_DB_URL;
 export const LOJAS_TOKENS = {
   LOJA_1_TOKEN: process.env.REACT_APP_LOJA_1_TOKEN,
   LOJA_2_TOKEN: process.env.REACT_APP_LOJA_2_TOKEN,
-  LOJA_2_TOKEN: process.env.REACT_APP_LOJA_3_TOKEN,
-  LOJA_2_TOKEN: process.env.REACT_APP_LOJA_4_TOKEN,
-  LOJA_2_TOKEN: process.env.REACT_APP_LOJA_5_TOKEN,
+  LOJA_3_TOKEN: process.env.REACT_APP_LOJA_3_TOKEN,
+  LOJA_4_TOKEN: process.env.REACT_APP_LOJA_4_TOKEN,
+  LOJA_5_TOKEN: process.env.REACT_APP_LOJA_5_TOKEN,
 };
